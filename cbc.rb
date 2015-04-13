cbc63c09c5c864b4e01ee9a02b9d6504a72e362e
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2015-04-13 15:13:28 +0900

    Add cbc.rb

---
+++ cbc.rb
@@ -0,0 +1,40 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Cbc < Formula
+  homepage ""
+  url "http://www.cr.ie.u-ryukyu.ac.jp/hg/CbC/CbC_llvm", using: :hg
+  version "llvm"
+  sha256 "b55dd4426265c52c517f79b2c79d0e556168c14c6ed5e79b51b6cf2f52f43e2a"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-optimized",
+                          "--enable-debug-runtime",
+                          "--enable-debug-symbols",
+                          "--enable-assertions"
+    # system "cmake", ".", *std_cmake_args
+    system "make -j 2" # if this fails, try separate make/make install steps
+    system "make install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test CbC`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
