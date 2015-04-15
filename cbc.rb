42e45a2aa7f30169c3082e43406e52b138abe1ba
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2015-04-15 21:39:02 +0900

    Delete test block

--- cbc.rb
+++ cbc.rb
@@ -24,17 +24,4 @@ class Cbc < Formula
     system "make", "install"
     bin.install_symlink "#{libexec}/bin/clang" => "cbc-clang"
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test CbC`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
