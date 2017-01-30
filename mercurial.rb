2c111256f97a307f67ffb2a14ed6d7d552bd4263
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2017-01-30 11:37:59 +0900

    Update mercurial to 4.0.2

--- mercurial.rb
+++ mercurial.rb
@@ -1,20 +1,25 @@
 class Mercurial < Formula
-  homepage "http://mercurial.selenic.com/"
-  url "http://mercurial.selenic.com/release/mercurial-3.3.3.tar.gz"
-  sha1 "999d5db4961e2c745f17df44d7ae64dce2d8425b"
+  desc "Scalable distributed version control system"
+  homepage "https://mercurial-scm.org/"
+  url "https://mercurial-scm.org/release/mercurial-4.0.2.tar.gz"
+  sha256 "ae6659dba27508a9a6417d535a89e282d5c8a5ea77b6e00af102822145b06d02"
 
   bottle do
-    root_url 'http://wolf.cr.ie.u-ryukyu.ac.jp/caches'
     cellar :any
-    sha256 "d8e1617d7b943d48d6e3db7d5792be749fa8aec4b3f5a6ff74ff1dc6f3253925" => :yosemite
-    sha256 "cc932a8efa6ed7b34c1a52b29833e1a77a57ee452159f8008bc890343c16af98" => :mavericks
-    sha256 "b9fbad5fcfd0e37496c613d3b9444d3c4ca66d7ab1deb9cf12c227c90d97d7ac" => :mountain_lion
+    root_url "http://ie.u-ryukyu.ac.jp/brew"
+    sha256 "20f193ea1672ab2126fe9de7055cc95f0115e49ba4a8b6d896f0e33d1cbf1691" => :sierra
+    sha256 "bf107e7f54ef8478ee6dd2268f96edce6ea6c54fce01f02e89795fd000442d97" => :el_capitan
+    sha256 "a5859696439b125ee70e98cd03ee44e75310f255a7ad1ecafe5db3945cf0b168" => :yosemite
   end
 
+  option "with-custom-python", "Install against the python in PATH instead of Homebrew's python"
+  if build.with? "custom-python"
+    depends_on :python
+  else
+    depends_on "python"
+  end
 
   def install
-    ENV.minimal_optimization if MacOS.version <= :snow_leopard
-
     system "make", "PREFIX=#{prefix}", "install-bin"
     # Install man pages, which come pre-built in source releases
     man1.install "doc/hg.1"
@@ -23,6 +28,20 @@ class Mercurial < Formula
     # install the completion scripts
     bash_completion.install "contrib/bash_completion" => "hg-completion.bash"
     zsh_completion.install "contrib/zsh_completion" => "_hg"
+  end
+
+  def caveats
+    return unless (opt_bin/"hg").exist?
+    cacerts_configured = `#{opt_bin}/hg config web.cacerts`.strip
+    return if cacerts_configured.empty?
+    <<-EOS.undent
+      Homebrew has detected that Mercurial is configured to use a certificate
+      bundle file as its trust store for TLS connections instead of using the
+      default OpenSSL store. If you have trouble connecting to remote
+      repositories, consider unsetting the `web.cacerts` property. You can
+      determine where the property is being set by running:
+        hg config --debug web.cacerts
+    EOS
   end
 
   test do
