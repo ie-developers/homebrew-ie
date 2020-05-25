cf770635dfa593673f7d6f7721c5d6bd0475c3cd
Shinji KONO <kono@ie.u-ryukyu.ac.jp>
2020-05-25 09:43:03 +0900

    cbc-llvm

--- cbc.rb
+++ cbc.rb
@@ -3,18 +3,28 @@
 
 class Cbc < Formula
   homepage "http://www.cr.ie.u-ryukyu.ac.jp"
-  url  "http://www.cr.ie.u-ryukyu.ac.jp/hg/CbC/CbC_llvm", using: :hg, revision: "21681fa9647e"
+  url  "http://www.cr.ie.u-ryukyu.ac.jp/hg/CbC/CbC_llvm", using: :hg, revision: "llvm10"
   head "http://www.cr.ie.u-ryukyu.ac.jp/hg/CbC/CbC_llvm", using: :hg
-  version "llvm3.8"
+  version "llvm10"
   sha256 "b55dd4426265c52c517f79b2c79d0e556168c14c6ed5e79b51b6cf2f52f43e2a"
+  depends_on"cmake"
 
   keg_only "Conflict with original clang"
 
   def install
     mktemp do
-      system "#{buildpath}/configure", "--prefix=#{prefix}", "--enable-assertions"
-      system "make -j 2"
-      system "make", "install"
+      STDERR.puts"before compile\n sudo launchctl stop com.apple.MRTd \n otherwise MRT may run crazy\n\this build may take 18GB"
+      system "cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_INSTALL_PREFIX:PATH=#{prefix} #{buildpath}"
+      system "ninja"
+      system "ninja", "install"
     end
   end
+
+  bottle do
+    rebuild 1
+    root_url "http://www.cr.ie.u-ryukyu.ac.jp/brew" # Optional root to calculate bottle URLs
+    sha256 "cd7ea217a174e440cfd7bf6e1367ceca7daae8f6ca9805056dd117e6cbc3ce97" => :mojave
+    sha256 "cd7ea217a174e440cfd7bf6e1367ceca7daae8f6ca9805056dd117e6cbc3ce97" => :catalina
+  end
+
 end
