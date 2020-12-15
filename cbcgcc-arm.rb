7ec7e2e4361abd60a98d09f87e5d55ad1c75da69
Shinji KONO <kono@ie.u-ryukyu.ac.jp>
2020-12-15 15:27:48 +0900

    ...

--- cbcgcc-arm.rb
+++ cbcgcc-arm.rb
@@ -14,7 +14,7 @@ class CbcgccArm < Formula
   depends_on "mpfr"
   depends_on "libmpc"
   depends_on "zstd"
-  depends_on "arm-none-eabi-gcc"
+  depends_on "ARMmbed/arm-none-eabi-gcc" # "arm-none-eabi-gcc"
 
   bottle do
     rebuild 1
