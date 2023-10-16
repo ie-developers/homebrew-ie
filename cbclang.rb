6a5c704b94f79d9e1b5d77afac17864a558f6c62
Shinji KONO <kono@ie.u-ryukyu.ac.jp>
2023-10-16 18:09:22 +0900

    ...

--- cbclang.rb
+++ cbclang.rb
@@ -17,10 +17,10 @@ class Cbclang < Formula
     sha256 cellar: :any, arm64_ventura: "0d792f6ae8f7cf39546d8d74a8ed6b78313a80e48b3e3b94af19c70677b3880f"
     sha256 cellar: :any_skip_relocation, catalina: "cea094bdedca36239812bdb8655806caa34194e0ccb181e06e0b945742d97569" 
     sha256 cellar: :any_skip_relocation, big_sur: "2ba57ee76a34d34db3424a2385a9ce3c49db1a17abada8fe533d8f618ea4b26d" 
-    sha256 cellar: :any_skip_relocation, mojave: "849051152676d540389fbe4029f430dc194bce4035936f6724761b2d7a0c1a70" 
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "934140268cd63de051b90f371a88ca6a9d4d44c32767b8278950abb3e5ea55d4" 
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9c962b229614a2b9975d228913b75c7310cb0206438c8273a0baa39a0e6885d7"
     sha256 cellar: :any_skip_relocation, ventura: "1d3160662cf70b494a8a9a5386031a6172e98510986929a8287db465bfde8a4d"
+    sha256 cellar: :any_skip_relocation, mojave: "28a43b892fa78325764d53cfe7bc2ce911fc9fda8b723bca56caf3e83ec710d3"
   end
 
   keg_only "conflict with original clang"
@@ -33,8 +33,7 @@ class Cbclang < Formula
   def install
     mktemp do
       $stderr.puts "before compile\n sudo launchctl stop com.apple.MRTd \n otherwise MRT may run crazy\nthis build may take 18GB\n"
-      #  CPU.intel is a workaround for bad llvm in intel mac
-      if MacOS.version >= :catalina && !Hardware::CPU.intel?
+      if MacOS.version >= :catalina 
         # llvm = Utils.safe_popen_read("brew", "--prefix", "llvm").chomp
         llvm = Formula["llvm"].prefix
         ENV["CC"] = "#{llvm}/bin/clang"
